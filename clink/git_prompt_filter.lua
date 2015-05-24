815aeb35ef7f717e779ee1d80276f562402007c8
Dan Wagner <danwagnerco@gmail.com>
2015-05-24 09:12:31 -0500

    Show git branch

---
+++ clink/git_prompt_filter.lua
@@ -0,0 +1,14 @@
+-- belongs here -> C:\Users\<your_user_name>\AppData\Local\clink\git_checkout.lua
+function git_prompt_filter()
+    for line in io.popen("git branch 2>nul"):lines() do
+        local m = line:match("%* (.+)$")
+        if m then
+            clink.prompt.value = "["..m.."] "..clink.prompt.value
+            break
+        end
+    end
+
+    return false
+end
+
+clink.prompt.register_filter(git_prompt_filter, 50)
