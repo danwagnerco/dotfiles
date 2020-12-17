4892695ee89de9f613b3551655ffa752e4f57a1a
Dan Wagner <dan@danwagner.co>
2020-12-17 08:27:56 -0600

    Add details on how to correctly create master.csv

--- bash/combine_csv_files.sh
+++ bash/combine_csv_files.sh
@@ -4,5 +4,5 @@
 # the single quote version works on Unix-y systems, use double quotes for Windows via cmder!
 awk 'FNR==1 && NR!=1{next;}{print}' *.csv > master.csv
 
-# Windows variant (far more common all things considered)
-awk "FNR==1 && NR!=1{next;}{print}" *.csv > master.csv
+# Windows variant (far more common all things considered) to prevent awk from repeatedly ingesting the same files
+awk "FNR==1 && NR!=1{next;}{print}" *.csv > c:\data\some_other_director\master.csv
