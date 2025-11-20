120824b91ab5b27619cfdffa837ae2651368faa5
Dan Wagner <dan@danwagner.co>
2025-11-19 19:37:36 -0600

    Configure PSReadLine for better autocomplete

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,13 @@
 #
 # Source the latest PowerShell config:
 # PS C:\github> . $PROFILE
+
+# Configure PSReadLine for more intuitive tab autocomplete
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView  # Shows multiple suggestions
+Set-PSReadLineKeyHandler -Key Tab -Function AcceptSuggestion # Tab accepts suggestion
+
+# Convenience keyboard shortcuts
 function eee {
     eza -l -a -h @args
 }
